d3a6584b6ee153a59d83581ada9de279d0a0298a
Victor Mathias <victormathias001@gmail.com>
2022-07-01 21:33:11 +0100

    Revert "Popup done"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,20 @@
-import React, {useState} from 'react'
-import Modal from './Modal'
-import { ReactDimmer } from 'react-dimmer';
+import React from 'react'
 
 const InputShortener = () => {
-    const [isModalOpen, setModal] = useState(false);
-
-    const handleClick = () => {
-        setModal((prevState) => !prevState);
-      };
     return (
         <div className='inputContainer'>
             <h1>URL <span>Shortener</span></h1>
             <div>
                 <input type="text" placeholder='Paste a link to shorten it' />
-                <button onClick={handleClick}>shorten</button>
+                <button>shorten</button>
             </div>
 
-            {isModalOpen && <Modal closeModal={setModal} />}
 
-            <ReactDimmer
-            isOpen={isModalOpen}
-            exitDimmer={setModal}
-            zIndex={1}
-            blur={0.5}
-            />
+
+
+
+
+
         </div>
     )
 }
